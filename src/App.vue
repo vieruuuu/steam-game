55681e12238a2de75e0bb5df40189e8a553f81ce
Vieru Andrei <mixerandr3w001@gmail.com>
2022-07-06 14:36:49 +0300

    Added quasar header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <HelloWorld :msg="msg" />
+  <div class="q-pa-md">
+    <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
+      <q-header elevated>
+        <q-toolbar>
+          <q-toolbar-title>{{ msg }}</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+            praesentium molestias a adipisci, dolore vitae odit, quidem
+            consequatur optio voluptates asperiores pariatur eos numquam rerum
+            delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
 
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import { ref } from "vue";
 import { invoke } from "@tauri-apps/api";
 
 const msg = ref("");
